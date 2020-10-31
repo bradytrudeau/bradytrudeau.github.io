672dde669b3edfccf5b8aeeed5ad46bdcf67c08b
Brady Trudeau <bradytrudeau@gmail.com>
2020-10-31 12:21:03 -0500

    Components completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css';
 class Footer extends Component {
   render() {
     return (
-      <div id="footer">
-            <h3>Footer</h3>
+      <div id="footer" className="footer">
+            <h5>Copyright © Brady Trudeau 2020</h5>
       </div>
     );
   }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import './Projects.css';
+import ReactPlayer from 'react-player';
+
 
 class Projects extends Component {
   render() {
     return (
-      <div id="projects">
-            <h3>Projects</h3>
+      <div id="projects" className="projects">
+            <h2 className="projectsH2">Projects</h2>
+            <ReactPlayer url="https://youtu.be/FPe3gaOlHdg" className="projectVideo"/>
+            <h3 className="projectsH3">Click! Metronome App</h3>
+            <p className="projectsCard">
+              Musician and marketing professional turned software developer. 
+              I'm a creative individual with a passion for technology. 
+              When I'm not geeking out on the newest effects pedals or building apps, 
+              you can find me playing with one of my bands, making food with my wife, 
+              or cheering on one of the many (usually disappointing) Minnesota sports teams. 
+            </p>
+
       </div>
     );
   }
