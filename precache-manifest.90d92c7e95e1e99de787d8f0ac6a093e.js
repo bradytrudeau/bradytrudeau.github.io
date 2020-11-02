2a241f712305a9c678ab518691c31df78a127b48
Brady Trudeau <bradytrudeau@gmail.com>
2020-11-01 20:23:06 -0600

    Updates

---
+++ precache-manifest.90d92c7e95e1e99de787d8f0ac6a093e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "67f5daec5751d3f631a5",
+    "url": "/static/css/main.d08839e2.chunk.css"
+  },
+  {
+    "revision": "67f5daec5751d3f631a5",
+    "url": "/static/js/main.946c2902.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c94f1faa9300e12ca02f",
+    "url": "/static/js/2.8860dd06.chunk.js"
+  },
+  {
+    "revision": "0afedc02b7badf2b090045d69f41acec",
+    "url": "/static/media/github.0afedc02.png"
+  },
+  {
+    "revision": "296bff79e700ae5795ed20509f7d86f5",
+    "url": "/static/media/family.296bff79.png"
+  },
+  {
+    "revision": "274ed9e5faf266233ffd8def649f36b2",
+    "url": "/static/media/WebsiteHeaderPhoto.274ed9e5.png"
+  },
+  {
+    "revision": "72c5bdfd09d201889bb35e4b27730c63",
+    "url": "/static/media/ProjectsPhoto.72c5bdfd.png"
+  },
+  {
+    "revision": "24d1d229eb6d9ad663100dac909fec5a",
+    "url": "/index.html"
+  }
+];
